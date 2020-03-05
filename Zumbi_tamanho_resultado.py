7feaee87935cb0d0731c1a83f067aefd243934b7
rcdmoreno <rcdmoreno@gmail.com>
2020-03-05 19:20:46 -0300

    alguns exercios de hoje

--- Zumbi_tamanho_resultado.py
+++ Zumbi_tamanho_resultado.py
@@ -1,8 +1,19 @@
-print('\t Calcular o quantos Digitos \n\t tem um número elevado ao outro\n')
-num = int(input('Digite o número desejado: '))
-elev = int(input('O número será elevado: '))
+linha = '- -'
+coluna = '|'
+print(linha * 500)
+print(coluna * 1)
+print('\t Calcular o quantos Digitos tem um número elevado ao outro\n')
+print(coluna * 500)
+print(coluna * 1)
+print('\t O valor elevado deve ser menor do que 100\n')
+
+print(coluna * 1)
+
+num = float(input('Digite o número desejado: '))
+
+elev = float(input('O número será elevado: '))
 while (elev > 100):
-    elev = int(input('O valor muito alto, digite novamente: '))
+    elev = float(input('O valor muito alto, digite novamente: '))
 valor = num ** elev
-print('O número %d'%num, 'elevado %d'%elev, 'ao será: %d' %valor)
+print('O número %f'%num, 'elevado %f'%elev, 'ao será: %f' %valor)
 print('A quantidade de caracters é :',len(str(valor)))
