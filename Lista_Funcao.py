8246c3b89d489cea0662ea1e2bad5c167674eea5
rcdmoreno <rcdmoreno@gmail.com>
2020-03-18 13:37:35 -0300

    atualização exercicios

---
+++ Lista_Funcao.py
@@ -0,0 +1,13 @@
+arr = [1,2,3,4,5]
+
+k = int(input('Número: '))
+
+def FindNumber(arr, k):
+    if num in arr:
+        print('Sim')
+    else:
+        print('Não')
+
+if _name_ == '_main_':
+
+FindNumber(arr,k)
---
+++ Num_Lista.py
@@ -0,0 +1,6 @@
+lista = [1,2,3,4,5]
+num = int(input('Digite Numero: '))
+i=1
+for i in 5:
+    lista.find.one()
+    
